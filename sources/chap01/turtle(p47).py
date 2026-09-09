fea3c5bf4fc544decf4a286b1d57ef9640816efc
soarstill <soarstill@gmail.com>
2026-09-09 21:11:44 +0900

    Add source files for chapters 2 and 3
    - Added chapter 2 source files including various Python scripts for exercises.
    - Added chapter 3 source files with additional Python scripts for exercises and examples.

--- sources/chap01/turtle(p47).py
+++ sources/chap01/turtle(p47).py
@@ -3,9 +3,14 @@ import turtle			# (1)
 t = turtle.Turtle()		# (2)
 t.shape("turtle")		# (3)
 
+t.color("blue")		# (4)
 t.forward(100)			# (4)
 t.left(90)			# (5)
 t.forward(50)
+t.backward(100)
+
+
+screen = turtle.Screen()
+screen.onclick(lambda x, y: screen.bye())
 	
 turtle.mainloop()		# (6)
-turtle.bye()	
--- sources/chap03/bool2.py
+++ sources/chap03/bool2.py
@@ -4,3 +4,6 @@ if  expensive :		# 관계 수식 대신에 부울 변수가 들어가도 된다.
 	shipping_cost = 0
 else :
 	shipping_cost = 3000
+
+total_cost = price + shipping_cost
+print("Total cost:", total_cost)
